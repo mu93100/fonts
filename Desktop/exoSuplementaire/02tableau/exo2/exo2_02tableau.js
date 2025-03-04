2b0626603ab827eac3ae7468733dae00b130128d
mu93100 <muriel.ehlinger@lepoles.org>
2025-03-04 10:24:39 +0100

    jjj

--- Desktop/exoSuplementaire/02tableau/exo2/exo2_02tableau.js
+++ Desktop/exoSuplementaire/02tableau/exo2/exo2_02tableau.js
@@ -6,21 +6,41 @@
 // Affiche à nouveau la liste de fruits disponibles.
 let fruits = [ 'Mangue', 'Raisin', 'Figue', 'Kiwi' ];
 
-function fru() {
+for (let i = 0; i < fruits.length; i++) {
     let listeDispo=document.createElement("li")
     document.body.appendChild(listeDispo)
     listeDispo.textContent=fruits
     let questionClient = document.createElement("input")
     
+    let reponsClient = prompt("Q U E L    F R U I T    V O U L E Z - V O U S    ?")
+    return
+
+if (reponsClient===fruits["i"]) {
+    fruits.splice(fruits.indexOf(i), 1)
+    console.log(fruits);
+    prompt("O K")
     
-    let reponsClient = prompt("V O U S   V O U L E Z     Q U E L    F R U I T   ?")
-    
-if (reponsClient!==fruits[i]) {
-    alert("N O U S    N'A V O N S    P A S     C E    F R U I T")
-    
+}  
+return
+if (reponsClient!==fruits["i"]) {
+    alert("N O U S    N'A V O N S    P A S     C E    F R U I T") 
 }
-// if (condition) {
-    
-// }    
+return
 }
-fru (fruits)
+
+console.log(fruits);
+let fruits = ['Mangue', 'Raisin', 'Figue', 'Kiwi'];
+ 
+ 
+ 
+let fruitDesire = prompt("Quel fruit désirez-vous acheter ?");
+ 
+let index = fruits.indexOf(fruitDesire);
+ 
+if (index !== -1) {
+  fruits.splice(index, 1);
+  document.write("ok!");
+} else {
+  document.write("indisponible...");
+}
+ 
